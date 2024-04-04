cfc58b97978b06be34502a0d5684a158c92d21c4
Artem <vsezdes021@gmail.com>
2024-04-04 18:41:38 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// завдання 1
+let result= 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+// завдання 2
+let email = "stankoartem20@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+// завдання 3
+const a = "My";
+const b = "name"
+const c = "is"
+const fullName = a + " " + b + " " + c + " Viktor";
+console.log(fullName);
+
+// завдання 4 
+const userName = prompt("Введіть своє ім`я");
+const payment = 500;
+const message = `Дякуємо, ${userName}! До сплати ${payment} гривень;`;
+alert(message);
